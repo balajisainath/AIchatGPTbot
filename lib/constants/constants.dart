db1031a171f1d6c8209a5b84aa30d10fe9afa749
balajisainath <sainathbalaji01@gmail.com>
2023-04-17 20:59:59 +0530

    "first commit"

---
+++ lib/constants/constants.dart
@@ -0,0 +1,22 @@
+import 'package:chatgptapi/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+
+Color scaffoldBackgroundColor=const Color(0xFFB6BAEA);
+Color cardcolor=const Color(0xFF515193);
+
+// List<String> models=[
+//   'Model1',
+//   'Model2',
+//   'Model3',
+//   'Model4',
+//   'Model5',
+//   'Model6',
+// ];
+//
+// List<DropdownMenuItem<String>>? get getModelsItem{
+//   List<DropdownMenuItem<String>>? modelsItem =List<DropdownMenuItem<String>>.generate(models.length, (index) => DropdownMenuItem(
+//       value:models[index],
+//       child: TextWidget( label: models[index],fontSize: 15,)));
+//
+//   return modelsItem;
+// }
